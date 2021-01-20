d113633f5295d06fc73fe5af60716e8fe7aac49c
184461L <184461L@CL1PC01.SEGSTUD.NYP.EDU.SG>
2021-01-20 09:52:47 +0800

    pushing customer class

--- src/java/sg/edu/nyp/Customer.java
+++ src/java/sg/edu/nyp/Customer.java
@@ -7,81 +7,137 @@
 package sg.edu.nyp;
 
 import java.io.Serializable;
+import java.sql.Date;
 
-//public class Customer implements Serializable {
-//    public final long serialVersionUID = -1L;
-//    public int customerId;
-//    private String fullname;
-//    private String email;
-//    private String addressline1;
-//    private String addressline2;
-//    private String postalcode;
-//    private String mobile;
-//    private String password;
-//
-//    public Customer() {
-//    }
-//
-//    public int getCustomerId() {
-//        return customerId;
-//    }
-//
-//    public void setCustomerId(int customerId) {
-//        this.customerId = customerId;
-//    }
-//    public String getFullName() {
-//        return fullname;
-//    }
-//
-//    public void setFullName(String fullname) {
-//        this.fullname = fullname;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getAddressLine1() {
-//        return addressline1;
-//    }
-//
-//    public void setAddressLine1(String addressline1) {
-//        this.addressline1 = addressline1;
-//    }
-//
-//    public String getAddressLine2() {
-//        return addressline2;
-//    }
-//
-//    public void setAddressLine2(String addressline2) {
-//        this.addressline2 = addressline2;
-//    }
-//
-//    public String getPostalCode() {
-//        return postalcode;
-//    }
-//
-//    public void setPostalCode(String postalcode) {
-//        this.postalcode = postalcode;
-//    }
-//
-//    public String getMobile() {
-//        return mobile;
-//    }
-//
-//    public void setMobile(String mobile) {
-//        this.mobile = mobile;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//}
+public class Customer implements Serializable {
+    public final long serialVersionUID = -1L;
+    private String NRICNo;
+    private String Title;
+    private String Name;
+    private String Email;
+    private String HomeAdd;
+    private String BillingAdd;
+    private String PassportNo;
+    private String PassportExpiry;
+    private String MobilePhone;
+    private Date DoB;
+    private String OfficePhone;
+    private String HomePhone;
+    private String KrisFlyer;
+    private String Password;
+
+    public String getNRICNo() {
+        return NRICNo;
+    }
+
+    public void setNRICNo(String NRICNo) {
+        this.NRICNo = NRICNo;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getHomeAdd() {
+        return HomeAdd;
+    }
+
+    public void setHomeAdd(String HomeAdd) {
+        this.HomeAdd = HomeAdd;
+    }
+
+    public String getBillingAdd() {
+        return BillingAdd;
+    }
+
+    public void setBillingAdd(String BillingAdd) {
+        this.BillingAdd = BillingAdd;
+    }
+
+    public String getPassportNo() {
+        return PassportNo;
+    }
+
+    public void setPassportNo(String PassportNo) {
+        this.PassportNo = PassportNo;
+    }
+
+    public String getPassportExpiry() {
+        return PassportExpiry;
+    }
+
+    public void setPassportExpiry(String PassportExpiry) {
+        this.PassportExpiry = PassportExpiry;
+    }
+
+    public String getMobilePhone() {
+        return MobilePhone;
+    }
+
+    public void setMobilePhone(String MobilePhone) {
+        this.MobilePhone = MobilePhone;
+    }
+
+    public Date getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(Date DoB) {
+        this.DoB = DoB;
+    }
+
+    public String getOfficePhone() {
+        return OfficePhone;
+    }
+
+    public void setOfficePhone(String OfficePhone) {
+        this.OfficePhone = OfficePhone;
+    }
+
+    public String getHomePhone() {
+        return HomePhone;
+    }
+
+    public void setHomePhone(String HomePhone) {
+        this.HomePhone = HomePhone;
+    }
+
+    public String getKrisFlyer() {
+        return KrisFlyer;
+    }
+
+    public void setKrisFlyer(String KrisFlyer) {
+        this.KrisFlyer = KrisFlyer;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+}
+    
+
+   
