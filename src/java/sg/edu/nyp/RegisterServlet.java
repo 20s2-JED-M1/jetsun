27aaa860252a94d79402ebb5989d21a418917ba2
chryslimjy <chrystal.limjy@gmail.com>
2021-01-27 10:33:39 +0800

    done up register servlet

--- src/java/sg/edu/nyp/RegisterServlet.java
+++ src/java/sg/edu/nyp/RegisterServlet.java
@@ -6,18 +6,29 @@
 package sg.edu.nyp;
 
 import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
 
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
 
+    @Resource(name = "jdbc/collabproj")
+    private DataSource dbRegister;
+
     public static String getSha256(String value) {
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(value.getBytes());
             return bytesToHex(md.digest());
         } catch (Exception ex) {
@@ -37,22 +48,193 @@ public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, java.io.IOException {
 
-        String nricno = request.getParameter("nric");
+        String nric = request.getParameter("nric");
         String title = request.getParameter("title");
         String name = request.getParameter("name");
         String email = request.getParameter("email");
-        String address1 = request.getParameter("address");
+        String address = request.getParameter("address");
         String passportno = request.getParameter("passportno"); //optional
         String passportexpiry = request.getParameter("expirydate");
         String mobile = request.getParameter("mobileno");
         String plainPassword = request.getParameter("password");
         String password = getSha256(plainPassword);
-        
+
         String dob = request.getParameter("dob");
         String officeno = request.getParameter("officeno");
         String homeno = request.getParameter("homeno");
         String memberno = request.getParameter("memberno");
         String billingaddress = request.getParameter("billingaddress");
+        
+        System.out.println(plainPassword + "checking password");
+
+        String sqlInsertAccFirstPart = "INSERT INTO customer (NRICNo, Title, Name, Email, HomeAdd";
+        String sqlInsertAccSecondPart = "VALUES (?, ?, ?, ?, ? ";
+
+        if (billingaddress != null && !billingaddress.isEmpty()) {
+            sqlInsertAccFirstPart += ",BillingAdd";
+            sqlInsertAccSecondPart += ", ?";
+        }
+
+        sqlInsertAccFirstPart += " ,PassportNo,PassportExpiry,MobilePhone";
+        sqlInsertAccSecondPart += ", ?, ?, ?";
+
+        if (dob != null && !dob.isEmpty()) {
+            sqlInsertAccFirstPart += ", DoB";
+            sqlInsertAccSecondPart += ", ?";
+        }
+        if (officeno != null && !officeno.isEmpty()) {
+            sqlInsertAccFirstPart += ", OfficePhone";
+            sqlInsertAccSecondPart += ", ?";
+        }
+        if (dob != null && !dob.isEmpty()) {
+            sqlInsertAccFirstPart += ", HomePhone";
+            sqlInsertAccSecondPart += ", ?";
+        }
+        if (dob != null && !dob.isEmpty()) {
+            sqlInsertAccFirstPart += ", KirsFlyer";
+            sqlInsertAccSecondPart += ", ?";
+        }
+
+        sqlInsertAccFirstPart += " ,password)";
+        sqlInsertAccSecondPart += ",?)";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultset = null;
+
+        try {
+            //Initialise the connection, statement and resultset  
+            connection = dbRegister.getConnection();
+
+            //Set auto commit to false to control the transaction
+            connection.setAutoCommit(false);
+            System.out.println("Front half + " + sqlInsertAccFirstPart);
+            System.out.println("Back half + " + sqlInsertAccSecondPart);
+            preparedStatement = connection.prepareStatement(sqlInsertAccFirstPart + sqlInsertAccSecondPart);
+            preparedStatement.setString(1, nric);
+            preparedStatement.setString(2, title);
+            preparedStatement.setString(3, name);
+            preparedStatement.setString(4, email);
+            preparedStatement.setString(5, address);
+
+            //Counter counts the number of question mark so that we can dynamically
+            //generate position where the variable is to be added
+            int counter = 6;
+
+            if (billingaddress != null && !billingaddress.isEmpty()) {
+                preparedStatement.setString(counter++, billingaddress);
+            }
+            
+            preparedStatement.setString(7, passportno);
+            preparedStatement.setString(8, passportexpiry);
+            preparedStatement.setString(9, mobile);
+            
+            
+            
+             int newCounter = 10;
+            
+            
+
+            if (dob != null && !dob.isEmpty()) {
+                preparedStatement.setString(newCounter++, dob);
+            }
+
+            if (officeno != null && !officeno.isEmpty()) {
+               preparedStatement.setString(newCounter++, officeno);
+            }
+            if (homeno != null && !homeno.isEmpty()) {
+                preparedStatement.setString(newCounter++, homeno);
+            }
+            
+            if (memberno != null && !memberno.isEmpty()) {
+                preparedStatement.setString(newCounter++, memberno);
+            }
+            
+            preparedStatement.setString(14, password);
+            
+            System.out.println("checking statement " + preparedStatement);
+
+            //Insert date into the database
+            preparedStatement.executeUpdate();
+
+            //If the code reaches this line, insertion must have been successful
+            //Commit the transaction
+            connection.commit();
+
+            //Leave a message in the session that the insertion is successful
+            // request.getSession().setAttribute("message","The account has been created successfully.");
+        } catch (SQLException ex) {
+            request
+                    .getSession()
+                    .setAttribute("message",
+                            "An error has occured in the insertion process, Please check with your DB administrator for more details.");
+            try {
+                //Roll back if there is an error
+                connection.rollback();
+            } catch (SQLException ex1) {
+                ex1.printStackTrace();
+                System.err.println(ex1.getMessage());
+            }
+            ex.printStackTrace();
+            System.err.println(ex.getMessage());
+        } finally {
+            //Resultset, Statement and Connection are closed in the finally 
+            // clause to ensure that they will be closed no matter what 
+            // happens to the system.
+            if (resultset != null) {
+                try {
+                    resultset.close();
+                } catch (SQLException ex) {
+                    try {
+                        //Roll back if there is an error
+                        connection.rollback();
+                    } catch (SQLException ex1) {
+                        ex1.printStackTrace();
+                        System.err.println(ex1.getMessage());
+                    }
+                    ex.printStackTrace();
+                    System.err.println(ex.getMessage());
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException ex) {
+                    try {
+                        //Roll back if there is an error
+                        connection.rollback();
+                    } catch (SQLException ex1) {
+                        ex1.printStackTrace();
+                        System.err.println(ex1.getMessage());
+                    }
+                    ex.printStackTrace();
+                    System.err.println(ex.getMessage());
+                }
+            }
+            if (connection != null) {
+                try {
+                    //Undo auto commit before closing the connection
+                    connection.setAutoCommit(true);
+                    connection.close();
+                } catch (SQLException ex) {
+                    try {
+                        //Roll back if there is an error
+                        connection.rollback();
+                    } catch (SQLException ex1) {
+                        ex1.printStackTrace();
+                        System.err.println(ex1.getMessage());
+                    }
+                    ex.printStackTrace();
+                    System.err.println(ex.getMessage());
+                }
+            }
+            //Make a client side redirect to the search result page
+
+            HttpSession session = request.getSession();
+            session.setAttribute("customerId", email);
+
+            response.sendRedirect(this.getServletContext().getContextPath() + "/menu.jsp");
+        }
 
     }
 
